4a9fea62369043f0809cfe73113f9b4b09887913
Hiroki Abe <hiroki.abe.58@gmail.com>
2022-06-30 23:16:43 +0900

    一式

---
+++ assets/js/include.js
@@ -0,0 +1,4 @@
+$(function(){
+  $('header').load("include/header.html");
+  $('#footer__bottom').load("include/footer.html");
+})
---
+++ assets/js/vue/vue.vue
@@ -0,0 +1,34 @@
+<script>
+var app = new Vue ({
+    el: "#app",
+    data: {
+        headCopy: "テキスト表示",
+        error_class: "error mb-3",
+        primary: "button-primary",
+        now : "00:00:00",
+        prefs:[
+            {name: '北海道'},
+            {name: '青森'},
+            {name: '岩手'},
+            {name: '宮城'},
+            {name: '秋田'},
+            {name: '山形'},
+            {name: '福島'},
+        ]
+    },
+    methods : {
+        time: function(e) {
+            var date = new Date();
+            this.now = 
+                date.getHours()+"時"
+                +date.getMinutes()+"分"
+                +date.getSeconds()+"秒";
+        },
+
+        shuffle: function() {
+            this.prefs =
+                _.shuffle(this.prefs);
+        }
+    }
+});
+</script>
